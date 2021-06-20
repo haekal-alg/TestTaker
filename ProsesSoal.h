32659a0adc554c4d7bcf843df227599fcb90bd00
haekal1002 <muhammadhaekal333@gmail.com>
2021-06-20 18:49:14 +0800

    Update display() to display_test()

--- ProsesSoal.h
+++ ProsesSoal.h
@@ -1,4 +1,5 @@
 #define SIZE 256 // besarnya buffer untuk struct
+#define ESC 27
 
 // untuk mempermudah penamaan, 
 // kumpulan soal dan jawaban dinamakan section
@@ -70,7 +71,11 @@ void section_to_struct(struct Soal_N_Jawaban *pointer_section, char filename[], 
 				else if (rem == 2) strcpy(temp.jawaban_a, line);
 				else if (rem == 3) strcpy(temp.jawaban_b, line);
 				else if (rem == 4) strcpy(temp.jawaban_c, line);
-				else if (rem == 0) strcpy(temp.jawaban_d, line);
+				else if (rem == 0){ 
+					// tambah newline pada baris terakhir di file
+					strcpy(temp.jawaban_d, line);
+					if (line_num == max_section*5) strcat(temp.jawaban_d, "\n");
+				}
 			}	
 			line_num++;
 		}
@@ -97,7 +102,7 @@ int is_file_valid(char filename[]){
 	
 	if (num_lines % 5 != 0){
 		printf("[-] File tidak valid! Pastikan nomor baris benar!\n");
-		printf("[-] Nomor baris %d.", num_lines);
+		printf("[*] Nomor baris %d.", num_lines);
 		return 0;
 	}
 	else{
@@ -109,6 +114,7 @@ int is_file_valid(char filename[]){
 		char cmp_jawaban[4][3] = {{"A."}, {"B."}, {"C."}, {"D."}};
 		char temp_jawaban[4][3];
 		
+		// cek tiap baris jawaban apakah sesuai dengan syarat
 		for (i = 1; i <= max_section; i++){
 			section_to_struct(&section, filename, i);
 			
@@ -127,7 +133,7 @@ int is_file_valid(char filename[]){
 			for (j = 0; j < 4; j++){
 				if (strcmp(temp_jawaban[j], cmp_jawaban[j]) != 0){
 					printf("[-] File tidak valid! Pastikan format soal benar!\n");
-					printf("[-] Cek baris %d.", (i-1)*5 + j + 1);
+					printf("[*] Cek baris %d.", (i-1)*5 + j + 1);
 					return 0;
 				}
 			}		
@@ -135,10 +141,64 @@ int is_file_valid(char filename[]){
 	}		
 }
 
+// fungsi untuk menampilkan soal sekaligus menerima jawaban,
+// berfungsi juga sebagai navigasi soal (berpindah dari satu soal ke soal yang lain)
+void display_test(char filename[]){
+	int i;
+	int num = 1;  // nomor soal sekarang;
+	char ch, container_jawaban[4][SIZE];
+	struct Soal_N_Jawaban section;
+	
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	
+	// jumlah soal ujian yang akan ditampilkan
+	int banyak_soal = count_lines(filename)/5; 
+	int *jwbn_sementara = (int*)calloc(banyak_soal, sizeof(int));
 
+	while(1){
+		section_to_struct(&section, filename, num);
+		strcpy(container_jawaban[0], section.jawaban_a); 
+		strcpy(container_jawaban[1], section.jawaban_b);
+		strcpy(container_jawaban[2], section.jawaban_c); 
+		strcpy(container_jawaban[3], section.jawaban_d); 
+		
+		printf("%s", section.soal);
+		for (i = 0; i < 4; i++){
+			// cek yang telah dipilih sebelumnya 
+			// jika ada yang telah dijawab maka akan dihighlight
+			int is_highlight = (jwbn_sementara[num] != 0 && jwbn_sementara[num] == i+1);
+			
+			if (is_highlight) SetConsoleTextAttribute(h, 12);
+			printf("   %s", container_jawaban[i]);			
+			if (is_highlight) SetConsoleTextAttribute(h, 7);
+				
+		}
+		printf("\n<<<\t\t\t\t\t\t\t\t\t\t>>>\n");	
 
-
-
-
-
+		ch = getch();
+		
+		// =============== NAVIGASI SOAL ===============
+		// lakukan navigasi jika dideteksi 1 atau 0
+		if (ch == 49)      // jika angka 1 ditekan maka program akan menampilkan soal sebelumnya
+			num--;         
+		else if (ch == 48) // jika angka 0 ditekan maka program akan menampilkan soal selanjutnya 
+			num++;         
+		
+		// limit agar tidak bisa berpindah kurang dari 1 
+		// atau melewati batas maks soal
+		if (num < 1) num = 1;
+		else if (num > banyak_soal) num = banyak_soal;
+		
+		// =============== INPUT JAWABAN ===============	
+		// mengecek setiap jawaban user
+		if ((ch >= 'a' && ch <= 'd') || (ch >= 'A' && ch <= 'D')){
+			if (ch == 'a' || ch == 'A') jwbn_sementara[num] = 1;
+			else if (ch == 'b' || ch == 'B') jwbn_sementara[num] = 2;	
+			else if (ch == 'c' || ch == 'C') jwbn_sementara[num] = 3;
+			else if (ch == 'd' || ch == 'D') jwbn_sementara[num] = 4;
+		}
+					
+		system("cls");
+	}
+}
 
--- soal.c
+++ soal.c
@@ -2,137 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#include <conio.h>
 #include "ProsesSoal.h"
-#define SIZE1 10 //jumlah soal ujian yang akan ditampilkan
-
-void display(char jwb_sementara[SIZE1][SIZE1], char filename[]); //function prototype
 
 int main(){
-	struct Soal_N_Jawaban section; 
 	char filename[] = "sample.txt";
 	
-	char jwb_sementara[SIZE1][SIZE1];
-	
 	if (is_file_valid(filename)){
-		// proses
-		display(jwb_sementara, filename); //memanggil fungsi display
+		display_test(filename);
 	}
-
+	
 	return 0;
 }
 
-//fungsi untuk menampilkan soal sekaligus menerima jawaban, berfungsi juga sebagai navigasi soal (berpindah dari satu soal ke soal yang lain)
-void display(char jwb_sementara[SIZE1][SIZE1], char filename[]){
-	struct Soal_N_Jawaban section;
-	int i;
-	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-	
-	//looping sebanyak jumlah soal
-	for(i=1; i<=SIZE1; i++){	
-		section_to_struct(&section, "sample.txt", i);
-		
-		//menampilkan soal sekaligus meminta input dari user berupa jawaban
-		printf("%s", section.soal);
-		printf("%s", section.jawaban_a);
-		printf("%s", section.jawaban_b);
-		printf("%s", section.jawaban_c);
-		printf("%s", section.jawaban_d);
-		if(i == SIZE1){
-			printf("\n");
-		}
-		printf("\nJawaban: ");
-		scanf("%s", &jwb_sementara[i]);
-		
-		while(strcmpi(jwb_sementara[i], "A") != 0 && strcmpi(jwb_sementara[i], "B") != 0 && strcmpi(jwb_sementara[i], "C") != 0 && strcmpi(jwb_sementara[i], "D") != 0){
-			printf("\nJAWABAN TIDAK VALID!!");
-			printf("\nMasukkan kembali jawaban: ");
-			scanf("%s", &jwb_sementara[i]);
-		}//error handling
-		
-		//mengecek setiap jawaban user, jawaban yang dipilih oleh user akan dihighlight
-		//jika jawaban yang diinput user adalah A maka pilihan jawaban A akan dihighlight
-		if(strcmpi(jwb_sementara[i], "A") == 0){
-			system("cls");
-			printf("%s", section.soal);
-			
-			SetConsoleTextAttribute(h,12);
-			printf("%s", section.jawaban_a);
-			
-			SetConsoleTextAttribute(h,7);
-			printf("%s", section.jawaban_b);
-			printf("%s", section.jawaban_c);
-			printf("%s", section.jawaban_d);
-		}
-		
-		//jika jawaban yang diinput user adalah B maka pilihan jawaban B akan dihighlight
-		else if(strcmpi(jwb_sementara[i], "B") == 0){
-			system("cls");
-			printf("%s", section.soal);
-			printf("%s", section.jawaban_a);
-			
-			SetConsoleTextAttribute(h,12);
-			printf("%s", section.jawaban_b);
-			
-			SetConsoleTextAttribute(h,7);
-			printf("%s", section.jawaban_c);
-			printf("%s", section.jawaban_d);
-		}
-		
-		//jika jawaban yang diinput user adalah C maka pilihan jawaban C akan dihighlight
-		else if(strcmpi(jwb_sementara[i], "C") == 0){
-			system("cls");
-			printf("%s", section.soal);
-			printf("%s", section.jawaban_a);
-			printf("%s", section.jawaban_b);
-			
-			SetConsoleTextAttribute(h,12);
-			printf("%s", section.jawaban_c);
-			
-			SetConsoleTextAttribute(h,7);
-			printf("%s", section.jawaban_d);
-		}
-		
-		//jika jawaban yang diinput user adalah D maka pilihan jawaban D akan dihighlight
-		else if(strcmpi(jwb_sementara[i], "D") == 0){
-			system("cls");
-			printf("%s", section.soal);
-			printf("%s", section.jawaban_a);
-			printf("%s", section.jawaban_b);
-			printf("%s", section.jawaban_c);
-			
-			SetConsoleTextAttribute(h,12);
-			printf("%s", section.jawaban_d);
-			
-			SetConsoleTextAttribute(h,7);
-		}
-		
-		if(i == SIZE1){
-			printf("\n");
-		}
-		printf("\n<<<<\t\t\t\t\t\t\t\t\t\t\t\t\t\t>>>>\n");
-		
-		int cek1 = 0; //variabel untuk looping dalam navigasi soal
-		int cek2; //variabel untuk mengecek tombol yang ditekan oleh user
-		
-		//navigasi soal berupa pilihan untuk pindah ke soal berikutnya atau pindah ke soal sebelumnya
-		while(cek1 == 0){
-			printf("\n\tTekan Angka 1 Untuk Lanjut Ke Soal Berikutnya Atau Tekan Angka 0 untuk Kembali Ke Soal Sebelumnya\n\t");
-			cek2 = getch();
-			switch(cek2){
-				case 49: //jika angka 1 pada keyboard ditekan maka akan keluar dari loop dan program akan menampilkan soal selanjutnya
-					cek1 = 1;
-					break;
-				case 48: //jika angka 0 pada keyboard ditekan maka akan keluar dari loop dan program akan menampilkan soal sebelumnya karena nilai counter (i) berkurang
-					i = i - 2;
-					cek1 = 1;
-					break;
-				default:
-					printf("INVALID INPUT!!\n");
-					break; //error handling, jika user memberikan input selain angka 1 dan 0 maka looping akan terus berlanjut
-			}
-		}
-		
-		system("cls"); //membersihkan layar sebelum berganti ke soal berikutnya
-	}
-}
