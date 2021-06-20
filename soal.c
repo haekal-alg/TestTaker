d59dad42131039bcf50ba5fade0e379372251e73
Alvitoikrm <85780874+Alvitoikrm@users.noreply.github.com>
2021-06-20 17:47:55 +0700

    Update soal.c

--- soal.c
+++ soal.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <time.h>
 #include <windows.h>
 #include <conio.h>
 #include "ProsesSoal.h"
+#include "WriteAndGrade.h"
 #define SIZE1 5 //jumlah soal ujian yang akan ditampilkan
 
 void display(char jwb_sementara[SIZE1][SIZE1], char filename[]); //function prototype
@@ -12,6 +15,8 @@ int main(){
 	struct Soal_N_Jawaban section; 
 	char filename[] = "sample.txt";
 	
+	int front = 0, back = 0;
+	char jawaban[SIZE1]
 	char jwb_sementara[SIZE1][SIZE1];
 	
 	if (is_file_valid(filename)){
