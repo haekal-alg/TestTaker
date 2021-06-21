a16c4d3531a1c81cf86c6b259ee08ea04b714cbf
haekal1002 <muhammadhaekal333@gmail.com>
2021-06-21 21:20:55 +0800

    Fix cursor flickering and add finish test confirmation

--- soal.c
+++ soal.c
@@ -5,39 +5,51 @@
 #include <time.h>
 #include <windows.h>
 #include <omp.h>
-#include "ProsesSoal.h"
+#include "headers/ProsesSoal.h"
+#include "headers/Login.h"
 //#include "WriteAndGrade.h"
 
 int main(){
+	//login_prompt(); 
+	
+	// ============== SETUP =================
+	int TIME = 2*60 + 2%60; // M : S
+	char filename[] = "sample.txt";
+	// ======================================
+	
 	//int nilai = 0;
 	//int front = 0, back = 0;
 	//char jawaban[SIZE1]
 	//char jwb_sementara[SIZE1][SIZE1];
 	
-	int TIME = 2*60 + 2%60; // M : S
-	char filename[] = "sample.txt";
-	
 	int ujian_berlangsung = 1;
 	int *FLAG = &ujian_berlangsung;
-	// current issue:
-	// 1. (possible) racing antara timer dan proses ujian
-	// 2. install clear screen pas enter di pencet
-	if (is_file_valid(filename)){
+
+	if (is_file_valid(filename))
+	{
 		#pragma omp parallel sections 
-		{
+		{	
+			#pragma single
+			HideCursor();
+			//login_prompt(); 
+			
 			#pragma omp section 
 	        { 
 	            timer(TIME, FLAG); 
 	        }
 	        #pragma omp section
-	        { // ############ PROSES UJIAN ###############
+	        { 
 	        	// sleep sementara supaya timer di print duluan
 	        	Sleep(100); 			
 				display_test(filename, FLAG); 
-				//Sleep(50); 
-				
-	        } // ############ PROSES UJIAN ###############	
+
+	        } 
 		}		
 	}
+	
+	
+	
 	return 0;
 }
+
+
