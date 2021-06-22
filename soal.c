c0a3915df056eaf469bb2b6f4a659297b6d81497
Alvitoikrm <85780874+Alvitoikrm@users.noreply.github.com>
2021-06-22 09:25:34 +0700

    Update soal.c

--- soal.c
+++ soal.c
@@ -1,26 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include <time.h>
 #include <windows.h>
-#include "ProsesSoal.h"
-#include "WriteAndGrade.h"
-
-#define SIZE1 5 //jumlah soal ujian yang akan ditampilkan
-
-void display(char jwb_sementara[SIZE1][SIZE1], char filename[]); //function prototype
+#include <omp.h>
+#include "headers/ProsesSoal.h"
+#include "headers/Login.h"
+//#include "WriteAndGrade.h"
 
 int main(){
+	//login_prompt(); 
+	float nilai;
+	queue *q;
+        q = malloc(sizeof(queue));
+        initialize(q);
+	// ============== SETUP =================
+	int TIME = 2*60 + 1%60; // M : S
 	char filename[] = "sample.txt";
-	int nilai = 0;
-	int front = 0, back = 0;
-	char jawaban[SIZE1]
-	char jwb_sementara[SIZE1][SIZE1];
 	
-	if (is_file_valid(filename)){
-		display_test(filename);
+	int ujian_berlangsung = 1;
+	int *FLAG = &ujian_berlangsung;
+	// ======================================
+	
+	// nomor 10 biasanya ke jawab sendiri (?)
+	if (is_file_valid(filename))
+	{
+		#pragma omp parallel 
+		{	
+			// selesaikan dulu login baru lanjut ke proses selanjutnya
+			#pragma omp single
+			login_prompt(); 
+			
+			// jalankan timer dan test secara bersamaan
+			#pragma omp single nowait
+			{
+				#pragma omp task
+				{ 
+		            timer(TIME, FLAG); 
+		        }
+		        #pragma omp task
+		        { 
+		        	// sleep sementara supaya timer di print duluan
+		        	Sleep(100); 			
+					display_test(filename, FLAG); 
+	
+		        } 	
+			}
+		}		
 	}
-	
 	return 0;
 }
